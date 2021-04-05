eb88778b4e6051c9c651986667bf72417c905a73
miriamdong <miriamdong@gmai.com>
2021-04-05 20:19:49 +0000

    add favorites data

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -3,4 +3,14 @@ INSERT INTO users (name, username, email, password, date)
 VALUES
 ('Miu Miu', 'miu', 'miu@miu.com', '123', NOW()),
 ('Wall E', 'walle', 'wall@e.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', NOW()),
-('Alice', 'Alice@gmail.com', 'alice', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', NOW());
+('Alice', 'alice','Alice@gmail.com',  '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', NOW());
+
+
+INSERT INTO users (name, username, email, password, date)
+    VALUES ('Dylan', 'Holland', 'kaitlynray@google.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', NOW()),
+    ('Mable', 'Perez', 'miaharrison@inbox.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', NOW()),
+    ('Edwin', 'Neal', 'coralittle@aol.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', NOW()),
+   ('Gene', 'Lopez', 'claireday@live.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', NOW()),
+    ('Josie', 'McCormick', 'peytonduffy@google.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', NOW()),
+    ('Sylvia', 'Elliott', 'lukesolomon@mail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', NOW()),
+   ('Alan', 'Norton', 'alainarutledge@icloud.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', NOW());
