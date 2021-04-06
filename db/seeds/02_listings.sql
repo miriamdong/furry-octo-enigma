9ff96310e34f49b8a214bce69153d3d4d2fa5035
miriamdong <miriamdong@gmai.com>
2021-04-06 01:12:17 +0000

    add two more listings

--- db/seeds/02_listings.sql
+++ db/seeds/02_listings.sql
@@ -1,9 +1,11 @@
 INSERT INTO listings (image_url, seller_id, title, created_at, featured, price, active)
-VALUES  ('url', 6, 'NEW Album', 'April 17, 2019', true, 50, true),
+VALUES
+('url', 6, 'NEW Album', 'April 17, 2019', true, 50, true),
         ('url', 6, 'On the Line', 'March 22, 2019', true, 30, true),
-        ('url', 7, 'thank u, next', 'February 8, 2019', true, 22, false),
-        ('url', 8, 'the underrated youth', 'October 18, 2019', true, 29, false),
+        ('url', 7, 'Thank u, next', 'February 8, 2019', true, 22, false),
+        ('url', 8, 'The underrated youth', 'October 18, 2019', true, 29, false),
         ('url', 8, 'Hollywood''s Bleeding', 'September 6, 2019', true, 22, true),
         ('url', 9, 'Manic', 'January 17, 2020', false, 27, true),
         ('url', 9, 'Circles', 'January 17, 2020', true, 30, true),
-        ('url', 9, 'Changes', 'February 14, 2020', false, 40, true);
+        ('url', 9, 'Changes', 'February 14, 2020', false, 40, true),
+        ('url', 2, 'Great Buy', 'January 01, 2020', false, 20, true),('url', 2, 'Rare', 'January 30, 2021', false, 100, true);
