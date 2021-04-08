0b502fc5759bf6d0c328a721c34df79c2098dd86
miriamdong <miriamdong@gmai.com>
2021-04-08 21:27:12 +0000

    add footer

--- db/seeds/03_artists.sql
+++ db/seeds/03_artists.sql
@@ -6,4 +6,6 @@ VALUES  ('Beyonce', 'pop', 'The Live Album'),
         ('Kate Bush', 'pop','2020'),
         ('BJÖRK', 'IDM','January'),
         ('Mac Miller','Rock', 'YES'),
-        ('U2', 'pop','Sup');
+        ('U2', 'pop','Sup'),
+        ('Deadmau5', 'EDM', 'Interscope Records'),
+        ('Billie Eilish','Electronic, Pop', 'Copycat');
