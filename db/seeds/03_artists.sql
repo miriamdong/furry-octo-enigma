719681f52b79fb783160e13256775e713392b364
miriamdong <miriamdong@gmai.com>
2021-04-08 19:51:52 +0000

    add more des

--- db/seeds/03_artists.sql
+++ db/seeds/03_artists.sql
@@ -1,9 +1,9 @@
 INSERT INTO artists(name, genre, publisher)
 VALUES  ('Beyonce', 'pop', 'The Live Album'),
-        ('Jenny Lewis', 'pop','LIVE'),
+        ('Pink Floyd', 'pop','LIVE'),
         ('Massive Attack','Downtempo', 'NATION'),
-        ('Yungblud', 'pop','unin'),
-        ('Post Malone', 'pop','2020'),
+        ('Portishead', 'indie','unin'),
+        ('Kate Bush', 'pop','2020'),
         ('BJÖRK', 'IDM','January'),
         ('Mac Miller','Rock', 'YES'),
         ('U2', 'pop','Sup');
